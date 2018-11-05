d22ca0c1e8b5bfce80dc3c02d7e055efde367d63
Rick Wong <>
2018-11-05 00:13:54 +0000

    Edit syntax for JavaScript binary search

--- javascript/search/binary_search.js
+++ javascript/search/binary_search.js
@@ -1,16 +1,13 @@
 // Array is assumed to be sorted for the purposes of binary search. Return -1 if search item is not present
-function binarySearch(array, searchItem) {
-    return recursiveSearch(0, array.length - 1, array, searchItem);
-}
+const binarySearch = (array, searchItem) => (
+    recursiveSearch(0, array.length - 1, array, searchItem)
+);
 
-function recursiveSearch(low, high, array, searchItem) {
-    if (high >= low) {
-        const mid = Math.floor((high + low) / 2);
-        if (array[mid] !== searchItem) {
-            if (array[mid] > searchItem) return recursiveSearch(low, mid - 1, array, searchItem);
-            return recursiveSearch(mid + 1, high, array, searchItem);
-        } 
-        return mid;
-    }
-    return -1;
-}
+const recursiveSearch = (low, high, array, searchItem) => {
+    if (high < low) return -1;
+    const mid = Math.floor((high + low) / 2);
+    if (array[mid] === searchItem) return mid;
+    return array[mid] > searchItem
+        ? recursiveSearch(low, mid - 1, array, searchItem)
+        : recursiveSearch(mid + 1, high, array, searchItem);
+};
