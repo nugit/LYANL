66c72d5a2bb7804fff334e6430339408c4a8405a
Bertrand31 <bertrandjun@gmail.com>
2019-04-18 12:05:48 +0800

    Improve bloom filter performance, add approxNumberOfItems method

--- scala/data_structures/bloom_filter.scala
+++ scala/data_structures/bloom_filter.scala
@@ -1,66 +1,78 @@
 package data_structures
 
+import scala.util.hashing.MurmurHash3.stringHash
+import Math.{abs, ceil, log, pow, round}
+
 case class BloomFilter[A](
   nbOfItems: Int,
   falsePositiveProbability: Float,
   private val array: Array[Boolean],
-  private val hashFunctions: Array[String => Int]
+  private val numberOfHashFunctions: Int,
+  private val hashSeed: Int
 ) {
+
+  private val hashFunctions: Stream[String => Int] =
+    (1 to numberOfHashFunctions)
+      .toStream
+      .map(i => (str: String) => abs(stringHash(str, hashSeed + i)) % array.length)
 
   def +=(item: A): BloomFilter[A] = {
     val itemString = item.toString
-    val hashes = this.hashFunctions.map(_(itemString))
-    val updatedArray = hashes.foldLeft(this.array)((acc, hash) => acc.updated(hash, true))
-    BloomFilter(nbOfItems, falsePositiveProbability, updatedArray, hashFunctions)
+    val hashes = hashFunctions.map(_(itemString))
+    val updatedArray = hashes.foldLeft(array)((acc, hash) => acc.updated(hash, true))
+    this.copy(array=updatedArray)
   }
 
   def ++=(items: Seq[A]): BloomFilter[A] = items.foldLeft(this)(_ += _)
 
-  def contains(item: A): Boolean = {
+  def mayContain(item: A): Boolean = {
     val itemString = item.toString
-    this.hashFunctions.forall(fn => this.array(fn(itemString)))
+    hashFunctions.forall(fn => array(fn(itemString)))
+  }
+
+  def approxNumberOfItems(): Int = {
+    val setBits = array.filter(identity).length.toDouble
+    val totalBits = array.length.toDouble
+    round(
+      (-totalBits / numberOfHashFunctions.toDouble) * log((1D - (setBits / totalBits)))
+    ).toInt
   }
 }
 
 object BloomFilter {
 
-  import Math.{abs, ceil, log, pow, round}
-
   private def getArraySize(nbOfItems: Int, falsePositiveProbability: Float): Int =
     ceil(
-      abs(nbOfItems * log(falsePositiveProbability)) / log(1 / pow(log(2), 2))
+      abs(nbOfItems * log(falsePositiveProbability)).toDouble / log(1D / pow(log(2), 2).toDouble)
     ).toInt
 
   private def getNumberOfHashFunctions(nbOfItems: Int, arraySize: Int): Int =
     round(
-      (arraySize / nbOfItems) * log(2)
+      (arraySize.toDouble / nbOfItems.toDouble) * log(2)
     ).toInt
 
   def apply[A](nbOfItems: Int, falsePositiveProbability: Float): BloomFilter[A] = {
-    import scala.util.Random
-    import scala.util.hashing.MurmurHash3.stringHash
-
     val arraySize = getArraySize(nbOfItems, falsePositiveProbability)
-    val array = Array.ofDim[Boolean](arraySize)
-
-    val hashFunctions: Array[String => Int] =
-      Array.ofDim[String => Int](getNumberOfHashFunctions(nbOfItems, arraySize)).map(_ => {
-        val seed = Random.nextInt
-        (string: String) => abs(stringHash(string, seed)) % arraySize
-      })
-    BloomFilter(nbOfItems, falsePositiveProbability, array, hashFunctions)
+    BloomFilter(
+      nbOfItems=nbOfItems,
+      falsePositiveProbability=falsePositiveProbability,
+      array=Array.ofDim[Boolean](arraySize),
+      numberOfHashFunctions=getNumberOfHashFunctions(nbOfItems, arraySize),
+      hashSeed=scala.util.Random.nextInt
+    )
   }
 }
 
 object BloomFilterTest {
 
   def main(args: Array[String]): Unit = {
-    val empty = BloomFilter[Int](4000, 0.0000001f)
+    val empty = BloomFilter[Int](4, 0.1f)
     val withOne = empty += 4
     val withThree = withOne ++= Vector(5, 2910)
-    println(withThree contains 4)
-    println(withThree contains 5)
-    println(withThree contains 2910)
-    println(withThree contains 211)
+    println(withThree mayContain 4)
+    println(withThree mayContain 5)
+    println(withThree mayContain 2910)
+    println(withThree mayContain 211)
+    println(withThree.approxNumberOfItems)
   }
 }
